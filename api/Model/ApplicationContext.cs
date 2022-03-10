be5568962c2ffffc77806ba052a7b9734ff0ef78
Yael Bakker <yv.bakker@student.han.nl>
2022-03-10 14:48:15 +0100

    Add Commits table with FK to repo

--- api/Model/ApplicationContext.cs
+++ api/Model/ApplicationContext.cs
@@ -5,10 +5,21 @@ namespace api.Model
     public class ApplicationContext : DbContext
     {
         public virtual DbSet<GitRepo> Repositories { get; set; }
-
+        public virtual DbSet<GitCommit> Commits { get; set; }
         public ApplicationContext(DbContextOptions options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<GitCommit>(e =>
+            {
+                e.HasOne(gitCommit => gitCommit.GitRepo)
+                    .WithMany()
+                    .HasForeignKey(gitCommit => gitCommit.GitRepoId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
         }
     }
 
