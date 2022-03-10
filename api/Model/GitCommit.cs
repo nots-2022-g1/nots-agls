a62f60053fa0bd21b16fc4552edad716de3b6a83
Yael Bakker <yv.bakker@student.han.nl>
2022-03-10 14:47:02 +0100

    Add GitCommit, GitLogParserGitCommit

---
+++ api/Model/GitCommit.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Model;
+
+public class GitCommit
+{
+    [Key]
+    public string Hash { get; set; }
+    public DateTime Date { get; set; }
+    public string Message { get; set; }
+    public int GitRepoId { get; set; }
+    public virtual GitRepo GitRepo { get; set; }
+}
+
+public class GitLogParserGitCommit
+{
+    public DateTime commit_date { get; set; }
+    public string commit_hash { get; set; }
+    public string message { get; set; }
+}
