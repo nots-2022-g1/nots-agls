c24239247277f7e68da9e7470035829fe23013ad
Yael Bakker <yv.bakker@student.han.nl>
2022-03-11 12:54:11 +0100

    Add migration for GitCommit

---
+++ api/Migrations/20220311104526_GitCommit.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class GitCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Label",
+                table: "Label");
+
+            migrationBuilder.RenameTable(
+                name: "Label",
+                newName: "Labels");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Labels",
+                table: "Labels",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Repositories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Naam = table.Column<string>(type: "text", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repositories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Commits",
+                columns: table => new
+                {
+                    Hash = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: false),
+                    GitRepoId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commits", x => x.Hash);
+                    table.ForeignKey(
+                        name: "FK_Commits_Repositories_GitRepoId",
+                        column: x => x.GitRepoId,
+                        principalTable: "Repositories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Commits_GitRepoId",
+                table: "Commits",
+                column: "GitRepoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commits");
+
+            migrationBuilder.DropTable(
+                name: "Repositories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Labels",
+                table: "Labels");
+
+            migrationBuilder.RenameTable(
+                name: "Labels",
+                newName: "Label");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Label",
+                table: "Label",
+                column: "Id");
+        }
+    }
+}
