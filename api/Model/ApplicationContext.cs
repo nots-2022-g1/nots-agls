77862d16438418a78595867b99fc72544d13f0cd
Yael Bakker <yv.bakker@student.han.nl>
2022-03-24 14:34:01 +0100

    Add FK relationships for LabeledData

--- api/Model/ApplicationContext.cs
+++ api/Model/ApplicationContext.cs
@@ -7,6 +7,8 @@ namespace api.Model
         public virtual DbSet<GitRepo> Repositories { get; set; }
         public virtual DbSet<GitCommit> Commits { get; set; }
         public virtual DbSet<Label> Labels { get; set; }
+        public virtual DbSet<LabeledData> LabeledData { get; set; }
+        public virtual DbSet<DataSet> DataSets { get; set; }
 
         public ApplicationContext(DbContextOptions options) : base(options)
         {
@@ -20,6 +22,18 @@ namespace api.Model
                 e.HasOne(gitCommit => gitCommit.GitRepo)
                     .WithMany()
                     .HasForeignKey(gitCommit => gitCommit.GitRepoId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<LabeledData>(e =>
+            {
+                e.HasOne(labeledData => labeledData.GitCommit)
+                    .WithMany()
+                    .HasForeignKey(labeledData => labeledData.GitCommitHash)
+                    .OnDelete(DeleteBehavior.NoAction);
+                e.HasOne(labeledData => labeledData.DataSet)
+                    .WithMany()
+                    .HasForeignKey(labeledData => labeledData.DataSetId)
                     .OnDelete(DeleteBehavior.Cascade);
             });
         }
