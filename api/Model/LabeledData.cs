87517e465614304d18edeeb9e7ae7cdd5ec4120d
Yael Bakker <yv.bakker@student.han.nl>
2022-03-24 14:30:20 +0100

    Change foreign key type to string

--- api/Model/LabeledData.cs
+++ api/Model/LabeledData.cs
@@ -4,7 +4,7 @@ public class LabeledData
 {
     public int Id { get; set; }
     public bool UseFul { get; set; }
-    public int GitCommitId { get; set; }
+    public string GitCommitHash { get; set; }
     public int DataSetId { get; set; }
     public virtual GitCommit GitCommit { get; set; }
     public virtual DataSet DataSet { get; set; }
