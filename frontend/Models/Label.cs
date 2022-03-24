653fc182afa7de7a8ff4a61fb75ae3816898f5df
iliass <iliass@iek.dev>
2022-03-24 11:02:49 +0100

    Add creation and update date to Label view

--- frontend/Models/Label.cs
+++ frontend/Models/Label.cs
@@ -6,6 +6,9 @@ public class Label
 {
     public int Id { get; set; }
     public string Name { get; set; } = default!;
+
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
 }
 
 public class LabelCreateDto
