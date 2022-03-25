6fcd53bec8b96d9eea748f34a5eb15ad428913d3
Yael Bakker <yv.bakker@student.han.nl>
2022-03-25 15:15:47 +0100

    Add GenericCrudModel

---
+++ api/Model/GenericCrudModel.cs
@@ -0,0 +1,9 @@
+namespace api.Model;
+
+public class GenericCrudModel : IGenericCrudModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastModifiedAt { get; set; }
+}
