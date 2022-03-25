1fd0cacd6dc3a2bd725921b290b188e883436984
iliass <iliass@iek.dev>
2022-03-25 19:31:58 +0100

    Fix: PR Feedback

--- frontend/Services/DatasetService.cs
+++ frontend/Services/DatasetService.cs
@@ -12,33 +12,33 @@ public class DatasetService : IDatasetService
     {
 
         httpClient.BaseAddress = new Uri(config.GetSection("MyAppSettings").GetValue<string>("ApiUrl"));
-        this._client = RestService.For<IDatasetService>(httpClient, new RefitSettings());
+        _client = RestService.For<IDatasetService>(httpClient, new RefitSettings());
     }
 
     public async Task<ApiResponse<Dataset>> Create(DatasetDto dataset)
     {
         Log.Information("TEST");
         Log.Information(dataset.Name);
-        return await this._client.Create(dataset);
+        return await _client.Create(dataset);
     }
 
     public async Task<List<Dataset>> Get()
     {
-        return await this._client.Get();
+        return await _client.Get();
     }
 
     public async Task<Dataset> GetById(int id)
     {
-        return await this._client.GetById(id);
+        return await _client.GetById(id);
     }
 
     public async Task<ApiResponse<Dataset>> Update(int id, DatasetDto dataset)
     {
-        return await this._client.Update(id, dataset);
+        return await _client.Update(id, dataset);
     }
 
     public async Task<ApiResponse<Dataset>> Delete(int id)
     {
-        return await this._client.Delete(id);
+        return await _client.Delete(id);
     }
 }
