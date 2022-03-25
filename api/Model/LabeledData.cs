f5c044efada3724304442aeb462a720042333d0c
Yael Bakker <yv.bakker@student.han.nl>
2022-03-25 09:58:51 +0100

    Add LabeledDataDto, rename UseFul to IsUseful

--- api/Model/LabeledData.cs
+++ api/Model/LabeledData.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Model;
 
 public class LabeledData
 {
+    [Key]
     public int Id { get; set; }
-    public bool UseFul { get; set; }
+    public bool IsUseful { get; set; }
     public string GitCommitHash { get; set; }
     public int DataSetId { get; set; }
     public virtual GitCommit GitCommit { get; set; }
     public virtual DataSet DataSet { get; set; }
+}
+
+public class LabeledDataDto
+{
+    public bool IsUseful { get; set; }
+    public string GitCommitHash { get; set; }
+    public int DataSetId { get; set; }
 }
