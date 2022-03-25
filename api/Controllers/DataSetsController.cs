1e1ac6a0c4fece59aea61ba5496106ebc8cc566e
Yael Bakker <yv.bakker@student.han.nl>
2022-03-25 10:02:38 +0100

    Implement DataSetsController based on GenericCrudController

--- api/Controllers/DataSetsController.cs
+++ api/Controllers/DataSetsController.cs
@@ -1,27 +1,32 @@
 using api.Model;
 using api.Services;
+using Mapster;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers;
 
-public class DataSetsController : GenericCrudController<DataSet>
+public class DataSetsController : GenericCrudController<DataSet, DataSetDto>
 {
     private readonly ILabeledDataService _dataService;
-    
+
     public DataSetsController(IGenericCrudService<DataSet> service, ILabeledDataService dataService) :
         base(service)
     {
         _dataService = dataService;
     }
 
-    public override Task<IActionResult> Post(DataSet entity)
+    public override async Task<IActionResult> Post(DataSetDto dto)
     {
-        throw new NotImplementedException();
+        var created = await _service.Create(dto.Adapt<DataSet>());
+        return Created($"/datasets/{created.Id}", created);
     }
 
-    public override Task<IActionResult> Put(int id, DataSet entity)
+    public override async Task<IActionResult> Put(int id, DataSetDto dto)
     {
-        throw new NotImplementedException();
+        var dataSet = dto.Adapt<DataSet>();
+        dataSet.Id = id;
+        var modified = await _service.Update(dataSet);
+        return Ok(modified);
     }
 
     [HttpGet("{id:int}/data")]
@@ -29,4 +34,29 @@ public class DataSetsController : GenericCrudController<DataSet>
     {
         return Ok(await _dataService.Get(id));
     }
+
+    [HttpPost("{id:int}/data")]
+    public async Task<IActionResult> PostData(int id, LabeledDataDto data)
+    {
+        var createdLabel = await _dataService.Create(data.Adapt<LabeledData>());
+        return Created($"/datasets/{id}/data/{createdLabel.Id}", createdLabel);
+    }
+
+    [HttpPut("{dataSetId:int}/data/{id:int}")]
+    public async Task<IActionResult> PutData(int dataSetId, int id, LabeledDataDto data)
+    {
+        data.DataSetId = dataSetId;
+        var labeledData = data.Adapt<LabeledData>();
+        labeledData.Id = id;
+        var modified = await _dataService.Update(labeledData);
+        return Ok(modified);
+    }
+
+    [HttpDelete("{dataSetId:int}/data/{id:int}")]
+    public async Task<IActionResult> DeleteData(int dataSetId, int id)
+    {
+        await _dataService.Delete(id);
+        return Ok();
+    }
+
 }
