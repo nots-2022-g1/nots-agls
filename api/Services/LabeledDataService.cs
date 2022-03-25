b760163c84e80d62ad39f1da091422e1cc27a92a
Yael Bakker <yv.bakker@student.han.nl>
2022-03-25 09:59:42 +0100

    Implement LabeledDataService

--- api/Services/LabeledDataService.cs
+++ api/Services/LabeledDataService.cs
@@ -1,26 +1,42 @@
 using api.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Services;
 
 public class LabeledDataService : ILabeledDataService
 {
-    public Task<IList<LabeledData>> Get(int dataSetId)
+    private readonly ApplicationContext _context;
+    private readonly DbSet<LabeledData> _dataRepository;
+
+    public LabeledDataService(ApplicationContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _dataRepository = context.Set<LabeledData>();
     }
 
-    public Task<LabeledData> Create(LabeledData labeledData)
+    public async Task<IList<LabeledData>> Get(int dataSetId)
     {
-        throw new NotImplementedException();
+        return await _dataRepository.Where(e => e.DataSetId.Equals(dataSetId)).ToListAsync();
     }
 
-    public Task<LabeledData> Update(LabeledData labeledData)
+    public async Task<LabeledData> Create(LabeledData data)
     {
-        throw new NotImplementedException();
+        var created = _dataRepository.Add(data);
+        await _context.SaveChangesAsync();
+        return created.Entity;
     }
 
-    public Task Delete(int id)
+    public async Task<LabeledData> Update(LabeledData data)
     {
-        throw new NotImplementedException();
+        var entity = _dataRepository.Attach(data);
+        entity.State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return entity.Entity;
+    }
+
+    public async Task Delete(int id)
+    {
+        _dataRepository.Remove(new LabeledData { Id = id });
+        await _context.SaveChangesAsync();
     }
 }
