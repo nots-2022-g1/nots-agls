7361818b1efe7c275550e10facc9952ab0f8d2c7
Yael Bakker <yv.bakker@student.han.nl>
2022-03-28 14:30:12 +0200

    Re-introduce Keyword, KeywordSet

--- api/Model/Keyword.cs
+++ api/Model/Keyword.cs
@@ -2,13 +2,27 @@ using System.ComponentModel.DataAnnotations;
 
 namespace api.Model;
 
-public class Keyword : GenericCrudModel
+public class Keyword : IGenericCrudModel
 {
-    public int KeywordsetId { get; set; }
-    public virtual GenericCrudModel KeywordsSet { get; set; }
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastModifiedAt { get; set; }
+    public int KeywordSetId { get; set; }
+    public virtual KeywordSet KeywordSet { get; set; }
 }
 
-public class KeywordDto : GenericCrudModelDto
+public class KeywordDto
 {
+    public string Name { get; set; }
+    public int KeywordSetId { get; set; }
+}
+
+public class KeywordResponseDto
+{
+    public string Name { get; set; }
     public int KeywordsetId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastModifiedAt { get; set; }
 }
---
+++ api/Model/KeywordSet.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Model;
+
+public class KeywordSet : IGenericCrudModel
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastModifiedAt { get; set; }
+}
+
+public class KeywordSetDto
+{
+    public string Name { get; set; }
+}
