9350a7c979b384b0c7d56f90c4c4f44ccd1028cb
Yael Bakker <yv.bakker@student.han.nl>
2022-04-01 11:15:46 +0200

    Add new Keyword, KeywordSet models

--- frontend/Models/KeywordSet.cs
+++ frontend/Models/KeywordSet.cs
@@ -1,16 +1,9 @@
 namespace frontend.Models;
 
-public class Keyword
+public class KeywordSet
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime LastModifiedAt { get; set; }
-    public int KeywordSetId { get; set; }
-}
-
-public class KeywordDto
-{
-    public string Name { get; set; }
-    public int KeywordSetId { get; set; }
 }
