b42f5a66a8ed6ff27ff4e2b7661addc6a0eeb4bb
Yael Bakker <yv.bakker@student.han.nl>
2022-04-07 09:52:24 +0200

    Add migrations for MatchedOnKeyword

---
+++ api/Migrations/20220406144012_MatchedOnKeyword.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class MatchedOnKeyword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MatchedOnKeyword",
+                table: "LabeledData",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MatchedOnKeyword",
+                table: "LabeledData");
+        }
+    }
+}
