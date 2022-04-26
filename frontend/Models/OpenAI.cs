755605af1839e841749e98dae48c4fd31df0b291
Thijmen Maus <thijmen@maus.nu>
2022-04-26 13:21:30 +0200

    add tsv export and custom command

--- frontend/Models/OpenAI.cs
+++ frontend/Models/OpenAI.cs
@@ -13,22 +13,17 @@ public class Choice
     public string finish_reason { get; set; }
 }
 
-public class CommitSummarization
+public class CommitResponse
 {
     public string commit { get; set; }
-    public string summarization { get; set; }
+    public string response { get; set; }
 }
 
 public class OpenAIExtractDTO
 {
     private string _prompt;
 
-    public string prompt
-    {
-        get => _prompt;
-        set => _prompt = $"The following text describes a change, extract the reason for the change: \n {value}";
-    }
-
+    public string prompt { get; set; } = "";
     public double temperature { get; set; } = 0.7;
     public int max_tokens { get; set; } = 120;
     public double top_p { get; set; } = 1.0;
