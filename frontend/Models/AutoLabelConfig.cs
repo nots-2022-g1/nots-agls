8754faf0ce92eb34585e1142082003c339b65cca
Yael Bakker <yv.bakker@student.han.nl>
2022-05-16 10:40:31 +0200

    Add ExcludeKeyword to AutolabelConfig

--- frontend/Models/AutoLabelConfig.cs
+++ frontend/Models/AutoLabelConfig.cs
@@ -5,4 +5,5 @@ public class AutoLabelConfig
     public int DatasetId { get; set; }
     public int GitRepoId { get; set; }
     public int KeywordSetId { get; set; }
+    public bool ExcludeKeyword { get; set; }
 }
