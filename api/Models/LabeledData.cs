7a8224532df14fd80172247042727eb3acf07fec
Yael Bakker <yv.bakker@student.han.nl>
2022-05-22 11:46:55 +0200

    Add MatchedOnKeyword to dto

--- api/Models/LabeledData.cs
+++ api/Models/LabeledData.cs
@@ -18,6 +18,7 @@ public class LabeledDataDto
     public bool IsUseful { get; set; }
     public string Message { get; set; }
     public int DatasetId { get; set; }
+    public string? MatchedOnKeyword { get; set; }
 }
 
 public class LabeledDataCreateDto
